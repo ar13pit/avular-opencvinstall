a4c0bc799ba58a8e85b318d8fd353e05e9b4d741
Arpit Aggarwal <ar13pit@gmail.com>
2018-08-29 17:00:05 +0200

    build process speedtest. Different flags added

--- opencvinstall.bash
+++ opencvinstall.bash
@@ -157,11 +157,22 @@ config_cmake()
     cd build
     cmake -D CMAKE_BUILD_TYPE=RELEASE \
         -D CMAKE_INSTALL_PREFIX=~/.venvs/cv \
-        -D INSTALL_PYTHON_EXAMPLES=ON \
-        -D INSTALL_C_EXAMPLES=OFF \
         -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-"${VERSION}"/modules \
         -D PYTHON_EXECUTABLE=~/.venvs/cv/bin/python \
-        -D BUILD_EXAMPLES=ON ..
+        -D BUILD_EXAMPLES=OFF \
+        -D BUILD_opencv_apps=OFF \
+        -D BUILD_DOCS=OFF \
+        -D BUILD_PERF_TESTS=OFF \
+        -D BUILD_TESTS=OFF \
+        -D INSTALL_PYTHON_EXAMPLES=OFF \
+        -D INSTALL_C_EXAMPLES=OFF \
+        -D WITH_TBB=ON \
+        -D WITH_OPENMP=ON \
+        -D WITH_IPP=ON \
+        -D WITH_NVCUVID=ON \
+        -D WITH_CUDA=ON \
+        -D WITH_CSTRIPES=ON \
+        -D WITH_OPENCL=ON ..
 
     echo
     echo "--------------------------------------------------------------------------"
